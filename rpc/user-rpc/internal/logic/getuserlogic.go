feeed6b9368f6a29413e72a8fb13f12fb7f618b9
jackjie20162 <610841147@qq.com>
2026-09-18 13:57:10 +0800

    feat: add user rpc get user logic

---
+++ rpc/user-rpc/internal/logic/getuserlogic.go
@@ -0,0 +1,28 @@
+package logic
+
+import (
+	"context"
+	"short-drama-recommend/rpc/user-rpc/internal/svc"
+	"short-drama-recommend/rpc/user-rpc/pb"
+)
+
+type GetUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{ctx: ctx, svcCtx: svcCtx}
+}
+
+func (l *GetUserLogic) GetUser(in *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	return &pb.GetUserResponse{
+		User: &pb.User{
+			Id:        in.Id,
+			Country:   "US",
+			Language:  "en",
+			Locale:    "en-US",
+			Timezone:  "UTC",
+		},
+	}, nil
+}
