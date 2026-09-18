cce10a00c0e8096eb8cc827ef6195e39c4064297
jackjie20162 <610841147@qq.com>
2026-09-18 23:01:33 +0800

    config: add Stripe and PayPal webhook secrets

--- rpc/payment-rpc/internal/config/config.go
+++ rpc/payment-rpc/internal/config/config.go
@@ -5,6 +5,6 @@ import "github.com/zeromicro/go-zero/zrpc"
 type Config struct {
  zrpc.RpcServerConf
  Mysql struct { DataSource string }
- Stripe struct { SecretKey string }
- Paypal struct { ClientID string; ClientSecret string; Env string }
+ Stripe struct { SecretKey string; WebhookSecret string }
+ Paypal struct { ClientID string; ClientSecret string; Env string; WebhookID string }
 }
