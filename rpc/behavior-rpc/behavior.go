4bd2012f23967d0c7709c034c78575a7311830f5
jackjie20162 <610841147@qq.com>
2026-09-18 21:43:28 +0800

    Fix behavior RPC dependency wiring

--- rpc/behavior-rpc/behavior.go
+++ rpc/behavior-rpc/behavior.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
 
 	"short-drama-recommend/rpc/behavior-rpc/internal/config"
 	"short-drama-recommend/rpc/behavior-rpc/internal/logic"
+	"short-drama-recommend/rpc/behavior-rpc/internal/svc"
 	"short-drama-recommend/rpc/behavior-rpc/pb"
 )
 
@@ -18,12 +18,14 @@ var configFile = flag.String("f", "etc/behavior.yaml", "the config file")
 func main() {
 	flag.Parse()
 	var c config.Config
-	if err := c.Load(*configFile); err != nil { panic(err) }
+	if err := c.Load(*configFile); err != nil {
+		panic(err)
+	}
+	svcCtx := svc.NewServiceContext(c)
 	server := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		pb.RegisterBehaviorServiceServer(grpcServer, logic.NewBehaviorServer(nil))
+		pb.RegisterBehaviorServiceServer(grpcServer, logic.NewBehaviorServer(svcCtx))
 	})
 	defer server.Stop()
 	fmt.Printf("starting behavior rpc at %s\n", c.ListenOn)
 	server.Start()
-	_ = net.IPv4len
 }
