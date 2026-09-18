7f7932189f9bcc6863b3ee8b5547dd85aab7974c
jackjie20162 <610841147@qq.com>
2026-09-18 21:43:13 +0800

    Wire behavior dependencies

---
+++ rpc/behavior-rpc/internal/svc/servicecontext.go
@@ -0,0 +1,24 @@
+package svc
+
+import (
+	"database/sql"
+
+	"short-drama-recommend/internal/cache"
+	"short-drama-recommend/internal/db"
+	"short-drama-recommend/internal/repository"
+	"short-drama-recommend/rpc/behavior-rpc/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+	DB *sql.DB
+	BehaviorRepo repository.BehaviorRepository
+	Redis *cache.Redis
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	database, err := db.OpenMySQL(c.Mysql.DataSource)
+	if err != nil { panic(err) }
+	r := cache.NewRedis(c.Redis.Host, c.Redis.Pass)
+	return &ServiceContext{Config:c, DB:database, BehaviorRepo:repository.NewMySQLBehaviorRepository(database), Redis:r}
+}
