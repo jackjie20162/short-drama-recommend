f337886c0cf3d444a9fc4f993c0cf41d81db0a68
jackjie20162 <610841147@qq.com>
2026-09-18 22:24:40 +0800

    Register drama admin RPC service

--- rpc/drama-rpc/drama.go
+++ rpc/drama-rpc/drama.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"short-drama-recommend/rpc/drama-rpc/internal/config"
-	"short-drama-recommend/rpc/drama-rpc/internal/logic"
-	"short-drama-recommend/rpc/drama-rpc/internal/svc"
-	"short-drama-recommend/rpc/drama-rpc/pb"
-	"github.com/zeromicro/go-zero/core/conf"
-	"github.com/zeromicro/go-zero/zrpc"
-	"google.golang.org/grpc"
+ "flag"
+ "fmt"
+ "short-drama-recommend/rpc/drama-rpc/internal/config"
+ "short-drama-recommend/rpc/drama-rpc/internal/logic"
+ "short-drama-recommend/rpc/drama-rpc/internal/svc"
+ "short-drama-recommend/rpc/drama-rpc/pb"
+ "github.com/zeromicro/go-zero/core/conf"
+ "github.com/zeromicro/go-zero/zrpc"
+ "google.golang.org/grpc"
 )
 
-func main() {
-	configFile := flag.String("f", "etc/drama.yaml", "the config file")
-	flag.Parse()
-	var c config.Config
-	conf.MustLoad(*configFile, &c)
-	ctx := svc.NewServiceContext(c)
-	server := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		pb.RegisterDramaServiceServer(grpcServer, logic.NewDramaServer(ctx))
-	})
-	defer server.Stop()
-	fmt.Printf("Starting drama rpc at %s...\n", c.ListenOn)
-	server.Start()
+func main(){
+ configFile:=flag.String("f","etc/drama.yaml","the config file")
+ flag.Parse()
+ var c config.Config
+ conf.MustLoad(*configFile,&c)
+ ctx:=svc.NewServiceContext(c)
+ server:=zrpc.MustNewServer(c.RpcServerConf,func(g *grpc.Server){
+  pb.RegisterDramaServiceServer(g,logic.NewDramaServer(ctx))
+  pb.RegisterDramaAdminServiceServer(g,logic.NewDramaAdminServer(ctx))
+ })
+ defer server.Stop()
+ fmt.Printf("Starting drama rpc at %s...\n",c.ListenOn)
+ server.Start()
 }
