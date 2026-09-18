efc821307129028abb7972d6a8654a0988218582
jackjie20162 <610841147@qq.com>
2026-09-18 22:24:04 +0800

    Wire drama admin repositories

--- rpc/drama-rpc/internal/svc/servicecontext.go
+++ rpc/drama-rpc/internal/svc/servicecontext.go
@@ -1,27 +1,21 @@
 package svc
 
 import (
-	"database/sql"
-
-	"short-drama-recommend/internal/db"
-	"short-drama-recommend/internal/repository"
-	"short-drama-recommend/rpc/drama-rpc/internal/config"
+ "database/sql"
+ "short-drama-recommend/internal/db"
+ "short-drama-recommend/internal/repository"
+ "short-drama-recommend/rpc/drama-rpc/internal/config"
 )
 
 type ServiceContext struct {
-	Config    config.Config
-	DB        *sql.DB
-	DramaRepo repository.DramaRepository
+ Config config.Config
+ DB *sql.DB
+ DramaRepo repository.DramaRepository
+ DramaAdminRepo repository.DramaAdminRepository
+ EpisodeRepo repository.EpisodeRepository
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	database, err := db.OpenMySQL(c.Mysql.DataSource)
-	if err != nil {
-		panic(err)
-	}
-	return &ServiceContext{
-		Config:    c,
-		DB:        database,
-		DramaRepo: repository.NewMySQLDramaRepository(database),
-	}
+ database,err:=db.OpenMySQL(c.Mysql.DataSource);if err!=nil{panic(err)}
+ return &ServiceContext{Config:c,DB:database,DramaRepo:repository.NewMySQLDramaRepository(database),DramaAdminRepo:repository.NewMySQLDramaAdminRepository(database),EpisodeRepo:repository.NewMySQLEpisodeRepository(database)}
 }
