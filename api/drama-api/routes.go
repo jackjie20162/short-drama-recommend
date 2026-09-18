40b86c970b8a138fdfa95390f67f88f2db36d51b
jackjie20162 <610841147@qq.com>
2026-09-18 21:50:11 +0800

    Register REST routes

---
+++ api/drama-api/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest"
+	"short-drama-recommend/api/drama-api/internal/handler"
+	"short-drama-recommend/api/drama-api/internal/svc"
+)
+
+func registerRoutes(server *rest.Server, svcCtx *svc.ServiceContext) {
+	h := handler.NewHandler(svcCtx)
+	server.AddRoutes([]rest.Route{
+		{Method:http.MethodGet, Path:"/api/v1/dramas/:id", Handler:h.GetDrama},
+		{Method:http.MethodGet, Path:"/api/v1/dramas", Handler:h.ListDrama},
+		{Method:http.MethodGet, Path:"/api/v1/feed", Handler:h.GetFeed},
+		{Method:http.MethodPost, Path:"/api/v1/behaviors", Handler:h.RecordBehavior},
+	})
+}
