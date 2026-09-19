ac1426625c726eb9ca592d6caf577eb2c57064f5
jackjie20162 <610841147@qq.com>
2026-09-19 10:37:58 +0800

    connect admin api to media rpc

--- api/drama-admin-api/internal/config/config.go
+++ api/drama-admin-api/internal/config/config.go
@@ -1,3 +1,3 @@
 package config
 import("github.com/zeromicro/go-zero/rest";"github.com/zeromicro/go-zero/zrpc")
-type Config struct{rest.RestConf;DramaRpc zrpc.RpcClientConf;PaymentRpc zrpc.RpcClientConf;Mysql struct{DataSource string};SettingsEncryptionKey string}
+type Config struct{rest.RestConf;DramaRpc zrpc.RpcClientConf;PaymentRpc zrpc.RpcClientConf;MediaRpc zrpc.RpcClientConf;Mysql struct{DataSource string};SettingsEncryptionKey string}
