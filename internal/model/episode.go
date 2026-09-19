0404f6510c702211195928de9dfe40cc82247904
jackjie20162 <610841147@qq.com>
2026-09-19 13:34:15 +0800

    feat: extend episode content model

--- internal/model/episode.go
+++ internal/model/episode.go
@@ -1,13 +1,14 @@
 package model
 
 type Episode struct {
- ID uint64
- DramaID uint64
- EpisodeNo uint32
- Title string
- DurationSeconds uint32
- VideoURL string
- PosterURL string
- IsPaid bool
- Status int8
+	ID              uint64
+	DramaID         uint64
+	EpisodeNo       uint32
+	Title           string
+	Description     string
+	DurationSeconds uint32
+	VideoURL        string
+	PosterURL       string
+	IsPaid          bool
+	Status          int8
 }
