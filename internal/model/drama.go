571a63d46b36089c4a38849b12ebb16bab9f38ce
jackjie20162 <610841147@qq.com>
2026-09-19 13:34:11 +0800

    feat: extend drama business model

--- internal/model/drama.go
+++ internal/model/drama.go
@@ -1,15 +1,24 @@
 package model
 
+import "time"
+
 type Drama struct {
 	ID             uint64
 	Title          string
+	Subtitle       string
 	Description    string
 	Cover          string
 	Country        string
 	Language       string
+	Genres         []string
+	Tags           []string
 	TotalEpisodes  uint32
 	IsPaid         bool
 	Status         int8
 	PriceCents     uint64
 	Currency       string
+	Popularity     float64
+	CompletionRate float64
+	PayRate        float64
+	PublishedAt    time.Time
 }
