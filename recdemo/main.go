2b3e7e0f5db3ff4fd866cd4a75e04436597b5f1f
jackjie20162 <610841147@qq.com>
2026-09-19 11:35:00 +0800

    feat(recdemo): add ES-ready short drama pre-ranking demo

---
+++ recdemo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/zeromicro/go-zero/rest"
+	"short-drama-recommend/recdemo/internal/config"
+	"short-drama-recommend/recdemo/internal/handler"
+	"short-drama-recommend/recdemo/internal/logic"
+	"short-drama-recommend/recdemo/internal/model"
+	"short-drama-recommend/recdemo/internal/es"
+	"short-drama-recommend/recdemo/internal/rank"
+)
+
+var configFile = flag.String("f", "etc/rec-api.yaml", "config file")
+
+func main() {
+	flag.Parse()
+	var c config.Config
+	if err := c.Load(*configFile); err != nil { panic(err) }
+
+	seeds := model.Seeds()
+	ranker := rank.New(c.Rank)
+	esClient := es.New(c.ES, ranker)
+	svc := logic.NewFeedLogic(seeds, esClient, ranker)
+	server := rest.MustNewServer(c.RestConf)
+	defer server.Stop()
+	handler.Register(server, svc)
+	fmt.Printf("rec-api listening on %s\n", c.Host)
+	server.Start()
+}
