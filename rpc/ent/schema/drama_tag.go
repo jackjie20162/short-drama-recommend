6d0c8465eb47187d88936cc71806c0f37eb8e10c
jackjie20162 <610841147@qq.com>
2026-09-19 17:00:37 +0800

    fix(ent): model existing composite join keys

--- rpc/ent/schema/drama_tag.go
+++ rpc/ent/schema/drama_tag.go
@@ -2,16 +2,29 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/index"
 )
 
 type DramaTag struct { ent.Schema }
 
+func (DramaTag) Annotations() []schema.Annotation {
+	return []schema.Annotation{field.ID("drama_id", "tag_id")}
+}
+
 func (DramaTag) Fields() []ent.Field {
 	return []ent.Field{
 		field.Uint64("drama_id"),
 		field.Uint64("tag_id"),
+	}
+}
+
+func (DramaTag) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("drama", Drama.Type).Required().Unique().Field("drama_id"),
+		edge.To("tag", Tag.Type).Required().Unique().Field("tag_id"),
 	}
 }
 
