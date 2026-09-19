9bf04ad9402e11f6368dd9f4a8c0a0c2c5aadcaa
jackjie20162 <610841147@qq.com>
2026-09-19 18:42:37 +0800

    refactor(config): standardize RPC dependency settings

--- rpc/media-rpc/internal/config/config.go
+++ rpc/media-rpc/internal/config/config.go
@@ -1,3 +1,19 @@
 package config
+
 import "github.com/zeromicro/go-zero/zrpc"
-type Config struct { zrpc.RpcServerConf; Mysql struct { DataSource string } }
+
+type Config struct {
+	zrpc.RpcServerConf
+	Mysql struct {
+		DataSource string
+	}
+	Storage struct {
+		Provider string
+		Bucket string
+		Endpoint string
+		Region string
+		AccessKey string
+		SecretKey string
+		PublicBaseURL string
+	}
+}
