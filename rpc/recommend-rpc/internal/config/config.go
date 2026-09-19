b4ce840d63a17e1712e1c3cc551a3cdbd8a3b3ea
jackjie20162 <610841147@qq.com>
2026-09-19 18:42:33 +0800

    refactor(config): standardize RPC dependency settings

--- rpc/recommend-rpc/internal/config/config.go
+++ rpc/recommend-rpc/internal/config/config.go
@@ -4,6 +4,17 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
-	Mysql struct { DataSource string }
-	CacheRedis struct { Host string; Pass string }
+	Mysql struct {
+		DataSource string
+	}
+	CacheRedis struct {
+		Host string
+		Pass string
+	}
+	Elasticsearch struct {
+		Address string
+		Username string
+		Password string
+		IndexPrefix string
+	}
 }
