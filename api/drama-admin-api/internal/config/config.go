e4957e532092a4ba66d11da26d9c950a12e58181
jackjie20162 <610841147@qq.com>
2026-09-19 10:20:59 +0800

    feat(settings): add settings encryption configuration

--- api/drama-admin-api/internal/config/config.go
+++ api/drama-admin-api/internal/config/config.go
@@ -1,3 +1,3 @@
 package config
 import("github.com/zeromicro/go-zero/rest";"github.com/zeromicro/go-zero/zrpc")
-type Config struct{rest.RestConf;DramaRpc zrpc.RpcClientConf;PaymentRpc zrpc.RpcClientConf}
+type Config struct{rest.RestConf;DramaRpc zrpc.RpcClientConf;PaymentRpc zrpc.RpcClientConf;Mysql struct{DataSource string};SettingsEncryptionKey string}
