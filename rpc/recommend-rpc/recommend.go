1c1a41482fe25f38513e56f2c900b4cbf805ccee
jackjie20162 <610841147@qq.com>
2026-09-19 18:42:58 +0800

    fix(rpc): load config consistently with go-zero

--- rpc/recommend-rpc/recommend.go
+++ rpc/recommend-rpc/recommend.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
 
@@ -18,7 +19,7 @@ var configFile = flag.String("f", "etc/recommend.yaml", "the config file")
 func main() {
 	flag.Parse()
 	var c config.Config
-	if err := c.Load(*configFile); err != nil { panic(err) }
+	conf.MustLoad(*configFile, &c)
 	svcCtx := svc.NewServiceContext(c)
 	server := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		pb.RegisterRecommendServiceServer(grpcServer, logic.NewRecommendServer(svcCtx))
