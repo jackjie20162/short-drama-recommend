40ad4c342ca0d19a00f80e365cc6113e471e4c82
jackjie20162 <610841147@qq.com>
2026-09-19 10:37:49 +0800

    generate media protobuf

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -22,5 +22,6 @@ generate_proto proto/drama_admin.proto rpc/drama-rpc/pb
 generate_proto proto/behavior.proto rpc/behavior-rpc/pb
 generate_proto proto/recommend.proto rpc/recommend-rpc/pb
 generate_proto proto/payment.proto rpc/payment-rpc/pb
+ generate_proto proto/media.proto rpc/media-rpc/pb
 
 echo "protobuf generation completed; API gateway code is preserved"
