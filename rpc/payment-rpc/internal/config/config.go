4c9ab66ad4257273e0df4e271e0f766974e5d26f
jackjie20162 <610841147@qq.com>
2026-09-19 18:42:35 +0800

    refactor(config): standardize RPC dependency settings

--- rpc/payment-rpc/internal/config/config.go
+++ rpc/payment-rpc/internal/config/config.go
@@ -3,8 +3,18 @@ package config
 import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
- zrpc.RpcServerConf
- Mysql struct { DataSource string }
- Stripe struct { SecretKey string; WebhookSecret string }
- Paypal struct { ClientID string; ClientSecret string; Env string; WebhookID string }
+	zrpc.RpcServerConf
+	Mysql struct {
+		DataSource string
+	}
+	Stripe struct {
+		SecretKey string
+		WebhookSecret string
+	}
+	Paypal struct {
+		ClientID string
+		ClientSecret string
+		Env string
+		WebhookID string
+	}
 }
