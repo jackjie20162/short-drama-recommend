d15877b4b81f206ca19e67d28e68aed5c60a6739
jackjie20162 <610841147@qq.com>
2026-09-19 10:36:54 +0800

    fix payment service context

---
+++ rpc/payment-rpc/internal/svc/service_context.go
@@ -0,0 +1,20 @@
+package svc
+
+import (
+    "database/sql"
+    "short-drama-recommend/internal/db"
+    "short-drama-recommend/rpc/payment-rpc/internal/config"
+)
+
+type ServiceContext struct {
+    Config config.Config
+    DB *sql.DB
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+    database, err := db.OpenMySQL(c.Mysql.DataSource)
+    if err != nil {
+        panic(err)
+    }
+    return &ServiceContext{Config: c, DB: database}
+}
