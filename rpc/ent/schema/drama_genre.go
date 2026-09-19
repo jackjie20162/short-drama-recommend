29241925410257873ecb72a85da0a32f948e7971
jackjie20162 <610841147@qq.com>
2026-09-19 17:00:34 +0800

    fix(ent): model existing composite join keys

--- rpc/ent/schema/drama_genre.go
+++ rpc/ent/schema/drama_genre.go
@@ -2,16 +2,29 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/index"
 )
 
 type DramaGenre struct { ent.Schema }
 
+func (DramaGenre) Annotations() []schema.Annotation {
+	return []schema.Annotation{field.ID("drama_id", "genre_id")}
+}
+
 func (DramaGenre) Fields() []ent.Field {
 	return []ent.Field{
 		field.Uint64("drama_id"),
 		field.Uint64("genre_id"),
+	}
+}
+
+func (DramaGenre) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("drama", Drama.Type).Required().Unique().Field("drama_id"),
+		edge.To("genre", Genre.Type).Required().Unique().Field("genre_id"),
 	}
 }
 
