3d2cb97cc3cdc1b88c5fb32ec062dae4758f034e
jackjie20162 <610841147@qq.com>
2026-09-19 18:42:56 +0800

    fix(rpc): load config consistently with go-zero

--- rpc/behavior-rpc/behavior.go
+++ rpc/behavior-rpc/behavior.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
 
@@ -18,9 +19,7 @@ var configFile = flag.String("f", "etc/behavior.yaml", "the config file")
 func main() {
 	flag.Parse()
 	var c config.Config
-	if err := c.Load(*configFile); err != nil {
-		panic(err)
-	}
+	conf.MustLoad(*configFile, &c)
 	svcCtx := svc.NewServiceContext(c)
 	server := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		pb.RegisterBehaviorServiceServer(grpcServer, logic.NewBehaviorServer(svcCtx))
