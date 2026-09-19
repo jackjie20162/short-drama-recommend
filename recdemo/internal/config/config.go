b2d4238846afe601168ef35c250fd886b655b9ff
jackjie20162 <610841147@qq.com>
2026-09-19 11:56:45 +0800

    feat(recommend): configure MMoE inference endpoint

--- recdemo/internal/config/config.go
+++ recdemo/internal/config/config.go
@@ -4,17 +4,7 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
-	ES struct {
-		Address string
-		Index string
-	}
-	Rank struct {
-		PopularityWeight float64
-		CompletionWeight float64
-		PayWeight float64
-		FreshnessWeight float64
-		SeenPenalty float64
-		RegionBoost float64
-		LangBoost float64
-	}
+	ES struct { Address string; Index string }
+	Rank struct { PopularityWeight float64; CompletionWeight float64; PayWeight float64; FreshnessWeight float64; SeenPenalty float64; RegionBoost float64; LangBoost float64 }
+	MMoE struct { Enabled bool; Address string; Model string }
 }
